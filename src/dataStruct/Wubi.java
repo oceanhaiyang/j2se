6afe2e8d97e6d1fe50325b87a4ee9fa262195ffc
oceanhaiyang <2456982193@qq.com>
2017-07-10 14:09:30 +0800

    wubi

---
+++ src/dataStruct/Wubi.java
@@ -0,0 +1,24 @@
+package com.haiyang.j2se;
+import java.util.Scanner;
+
+/**
+ * Created by lhy on 2017/7/6.
+ */
+public class Wubi {
+    public static void main(String args[]){
+        Scanner in  = new Scanner(System.in);
+        String b =in.next();
+        //char[] codeArr = b.toCharArray();
+        int res= codeIndex(b);
+        System.out.println(res);
+    }
+    public static int codeIndex(String code){
+        int factor[]={1+25*25*25+25*25+25,1+25+25*25,1+25,1};
+        char[] a=code.toCharArray();
+        int index=0,len=0;
+        for(int i=0;i<a.length;i++) {
+            index += factor[len++] * (a[i] - 'a');
+        }
+        return index+(len-1);
+    }
+}
