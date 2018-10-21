a801d43b390f4b2e6e8a5631678ad19883bceaa1
haiyang <2456982193@qq.com>
2018-10-21 23:07:24 +0800

    findCelebrity

--- src/array/BattleshipsInABoard.java
+++ src/array/BattleshipsInABoard.java
@@ -33,29 +33,21 @@ public class BattleshipsInABoard {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        char[][] board = {{'X', '.', '.', 'X'}, {'.', '.', '.', 'X'}, {'.', '.', '.', 'X'}};
+        char[][] board = {{'.', '.', '.','.','.','.', 'X'}, {'X','X', 'X','.', 'X','.', 'X'}, {'.', '.', '.','.', '.', '.', 'X'}};
         System.out.println(new BattleshipsInABoard().countBattleships(board));
     }
 
     public int countBattleships(char[][] board) {
-        int count = 0;
+        int res = 0;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 'X') {
-                    if (i - 1 >= 0) { //check for the boundary condition
-                        if (board[i - 1][j] == 'X')
-                            continue;
-                    }
-                    if (j - 1 >= 0) {
-                        if (board[i][j - 1] == 'X') {
-                            continue;
-                        }
-                    }
-                    count++;
-                }
+                if (board[i][j] == '.') continue;
+                if (i > 0 && board[i - 1][j] == 'X') continue;
+                if (j > 0 && board[i][j - 1] == 'X') continue;
+                res++;
             }
         }
-        return count;
+        return res;
     }
 
 }
