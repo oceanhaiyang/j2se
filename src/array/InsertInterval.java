037489ac75aa957aa66a0e8f8d01d41b1dc45b4d
haiyang <2456982193@qq.com>
2018-10-28 22:55:52 +0800

    IncreasingTripletSubsequence

--- src/array/InsertInterval.java
+++ src/array/InsertInterval.java
@@ -5,12 +5,9 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
- You may assume that the intervals were initially sorted according to their start times.
- Example 1:
- Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
- Example 2:
- Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+ * Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary)
+  [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+          [4,          9]
  This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
  Solution: O(n): Iterate through each interval and check for each overlapping case
  */
@@ -34,48 +31,30 @@ public class InsertInterval {
         Interval i4 = new Interval(8, 10);
         Interval i5 = new Interval(12, 16);
         List<Interval> list = Arrays.asList(i1, i2, i3, i4, i5);
-        List<Interval> result = new InsertInterval().insert(list, new Interval(2, 5));
+        List<Interval> result = new InsertInterval().insert(list, new Interval(4, 9));
         result.stream().map(x -> x.start + " " + x.end).forEach(System.out::println);
     }
 
     public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
-        List<Interval> result = new ArrayList<>();
-        for(int i = 0, l = intervals.size(); i < l; i ++){
-            Interval curr = intervals.get(i);
-            if(newInterval.start >= curr.start && newInterval.end <= curr.end){
-                insertRest(intervals, result, i);
-                return result; //easy case
-            } else if(newInterval.start < curr.start && newInterval.end > curr.end){
-                newInterval = new Interval(newInterval.start, newInterval.end);
-                //merge and continue
-            } else if(newInterval.start >= curr.start && newInterval.start <= curr.end
-                    && newInterval.end > curr.end){
-                newInterval = new Interval(curr.start, newInterval.end);
-                //merge and continue
-            } else if(newInterval.start < curr.start && newInterval.end >= curr.start
-                    && newInterval.end <= curr.end){
-                Interval newI = new Interval(newInterval.start, curr.end);
-                result.add(newI);
-                insertRest(intervals, result, i + 1);
-                return result;
-            } else if(newInterval.start > curr.end){
-                result.add(curr);
-            } else {
-                result.add(newInterval);
-                insertRest(intervals, result, i);
-                return result;
-            }
+        if (newInterval == null) return intervals;
+        List<Interval> res = new ArrayList<>();
+        int i = 0;
+        //[1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+         //       [4,          9]
+        //无交集时直接加入到新的集合
+        while (i < intervals.size() && intervals.get(i).end < newInterval.start) {
+            res.add(intervals.get(i++));
         }
-        result.add(newInterval);
-        return result;
-    }
-
-    private void insertRest(List<Interval> intervals, List<Interval> result, int i){
-        int l = intervals.size();
-        while(i < l){
-            result.add(intervals.get(i));
+        //有交集时取首尾的节点
+        while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+            newInterval.start = Math.min(newInterval.start, intervals.get(i).start);
+            newInterval.end = Math.max(newInterval.end, intervals.get(i).end);
             i++;
         }
+        res.add(newInterval);
+        while (i < intervals.size()) {
+            res.add(intervals.get(i++));
+        }
+        return res;
     }
-
 }
